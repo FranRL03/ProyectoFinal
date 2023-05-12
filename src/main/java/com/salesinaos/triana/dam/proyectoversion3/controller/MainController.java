47d3e708ccfc1015a10cd4216bf6ac9f51d0f293
Fran Ruiz <fjavierrule@gmail.com>
2023-05-12 10:47:48 +0200

    seguridad mas o menos hecha

--- src/main/java/com/salesinaos/triana/dam/proyectoversion3/controller/MainController.java
+++ src/main/java/com/salesinaos/triana/dam/proyectoversion3/controller/MainController.java
@@ -14,6 +14,6 @@ public class MainController {
 	@GetMapping("/")
 	public String index() {
 		return "Principal";
-	}
+	} 
 
 }
--- src/main/java/com/salesinaos/triana/dam/proyectoversion3/service/UsuarioServicio.java
+++ src/main/java/com/salesinaos/triana/dam/proyectoversion3/service/UsuarioServicio.java
@@ -1,5 +1,7 @@
 package com.salesinaos.triana.dam.proyectoversion3.service;
 
 public class UsuarioServicio {
+	
+	
 
 }
