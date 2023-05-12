a9c21e37c02147a33e5705f1fc61de4549f99a1b
Fran Ruiz <fjavierrule@gmail.com>
2023-05-12 14:24:53 +0200

    entrega parcial

--- src/main/java/com/salesinaos/triana/dam/proyectoversion3/repo/LineaVentaRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.salesinaos.triana.dam.proyectoversion3.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesinaos.triana.dam.proyectoversion3.model.LineaVenta;
-
-public interface LineaVentaRepositorio extends JpaRepository<LineaVenta, Long>{
-
-}
