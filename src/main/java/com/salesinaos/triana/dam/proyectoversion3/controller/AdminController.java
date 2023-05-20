bbdd1d48037be3003a62e464450ed01de188683b
Fran Ruiz <fjavierrule@gmail.com>
2023-05-20 17:21:34 +0200

    guardado excepcion terminada

--- src/main/java/com/salesinaos/triana/dam/proyectoversion3/controller/AdminController.java
+++ src/main/java/com/salesinaos/triana/dam/proyectoversion3/controller/AdminController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.salesinaos.triana.dam.proyectoversion3.model.Hermano;
 import com.salesinaos.triana.dam.proyectoversion3.service.HermanoServicio;
-import com.salesinaos.triana.dam.proyectoversion3.service.ProductoServicio;
 
 @Controller
 public class AdminController {
@@ -90,58 +89,9 @@ public class AdminController {
 	@ModelAttribute("cuota_total")
 	public Double cuotaTotal() {
 		
-		hermanoServicio.sumarCuotas();
+	return hermanoServicio.sumarCuotas();
 		
-		return 0.0;
-	}
-	
-	
-	//=====================================================================================
-	
-	@GetMapping("/adminPro")
-	public String listarTodosProductos (Model model) {
-		model.addAttribute("products", productoServicio.findAll());
-		return "AdminProductos";
-	}
-	
-	@GetMapping("/delete/{id}")
-	public String borrarProducto(@PathVariable("id") long id) {
-		productoServicio.restarCantidadProducto(id, 1);
-		return "redirect:/adminPro";
-	}
-	
-	@GetMapping("/nuevoProducto")
-	public String mostrarFormularioProducto(Model model) {
-		model.addAttribute("producto", new Producto());
-		
-		return "FormularioNuevoProducto";
-	}
-	
-	@PostMapping("/addProducto")
-	public String procesarFormularioProducto(@ModelAttribute("producto") Producto p) {
-		productoServicio.add(p);
-		return "redirect:/adminPro";
-	}
-	
-	@GetMapping("/editarProducto/{id}")
-	public String mostrarFormularioEdicionProducto(@PathVariable("id") long id, Model model) {
-		
-		Producto editPro = productoServicio.findById(id);
-		
-		if (editPro != null) {
-			model.addAttribute("producto", editPro);
-			return "FormularioNuevoProducto";
-		} else {
-			return "redirect:/adminPro";
-		}	
 		
 	}
-	
-	@PostMapping("/editarProducto")
-	public String procesarFormularioEdicion(@ModelAttribute("producto") Producto p) {
-		productoServicio.edit(p);
-		return "redirect:/adminPro";
-	}
-	
 
 }
