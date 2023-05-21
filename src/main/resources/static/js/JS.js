7a6508eaa91906547620a5065c0f5758557299ad
Fran Ruiz <fjavierrule@gmail.com>
2023-05-21 22:58:33 +0200

    ultimando ultimas cosas

---
+++ src/main/resources/static/js/JS.js
@@ -0,0 +1,17 @@
+function login() {
+    let user, pass
+
+    user = document.getElementById("username").value;
+    pass = document.getElementById("password").value;
+
+    if (user == "user" && pass == "1234") {
+        window.location = "Principal.html"
+    }if (user == "admin" && pass == "admin") {
+        window.location = "login.html"
+    } 
+
+}
+
+
+
+
